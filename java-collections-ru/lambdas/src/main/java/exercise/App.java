df1cfcee15969f0db78a102bf0fe2d562569e19f
@hexlet/cli <support@hexlet.io>
2023-05-27 17:30:33 +0300

    submit java-collections-ru/lambdas

---
+++ java-collections-ru/lambdas/src/main/java/exercise/App.java
@@ -0,0 +1,20 @@
+package exercise;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+// BEGIN
+class App {
+    public static String[][] enlargeArrayImage(String[][] image) {
+        return Arrays.stream(image)
+                .flatMap(array -> Stream.of(duplicate(array), duplicate(array)))
+                .toArray(String[][]::new);
+    }
+
+    private static String[] duplicate(String[] line) {
+        return Arrays.stream(line)
+                .flatMap(element -> Stream.of(element, element))
+                .toArray(String[]::new);
+    }
+}
+// END
