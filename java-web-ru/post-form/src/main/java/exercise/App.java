2ae566d2e2e09527d7921138979d1f0f6014eb30
@hexlet/cli <support@hexlet.io>
2024-02-18 20:39:02 +0300

    submit java-web-ru/post-form

---
+++ java-web-ru/post-form/src/main/java/exercise/App.java
@@ -0,0 +1,61 @@
+package exercise;
+
+import io.javalin.Javalin;
+import java.util.List;
+import java.util.Collections;
+import exercise.model.User;
+import exercise.dto.users.UsersPage;
+import exercise.repository.UserRepository;
+import org.apache.commons.lang3.StringUtils;
+import exercise.util.Security;
+
+public final class App {
+
+    public static Javalin getApp() {
+
+        var app = Javalin.create(config -> {
+            config.plugins.enableDevLogging();
+        });
+
+        app.get("/", ctx -> {
+            ctx.render("index.jte");
+        });
+
+        app.get("/users", ctx -> {
+            List<User> users = UserRepository.getEntities();
+            var page = new UsersPage(users);
+            ctx.render("users/index.jte", Collections.singletonMap("page", page));
+        });
+
+        // BEGIN
+        app.get("/users/build", ctx -> {
+            ctx.render("users/build.jte");
+        });
+
+        app.post("/users", ctx -> {
+            var firstName = ctx.formParam("firstName").toLowerCase();
+            var capitalizedFirstName = StringUtils.capitalize(firstName);
+
+            var lastName = ctx.formParam("lastName").toLowerCase();
+            var capitalizedLastName = StringUtils.capitalize(lastName);
+
+            var email = ctx.formParam("email").trim().toLowerCase();
+
+            var password = ctx.formParam("password");
+            var passwordEncrypt = Security.encrypt(password);
+            var passwordConfirmation = ctx.formParam("passwordConfirmation");
+
+            var user = new User(capitalizedFirstName, capitalizedLastName, email, passwordEncrypt);
+            UserRepository.save(user);
+            ctx.redirect("/users");
+        });
+        // END
+
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}
